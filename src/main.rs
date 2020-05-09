56947c61965fe633b78cdc91bbb3e86d66307c48
Tomer Keren <tomer.keren.dev@gmail.com>
2020-05-09 22:53:03 +0300

    Implement vec3

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
 mod vec3;
 use itertools::Itertools;
+use vec3::Vec3;
 
 fn main() {
     let (width, height) = (200, 100);
     println!("P3\n{} {}\n255", width, height);
-    let a = crate::vec3::create([1, 1, 1]);
     for (j, i) in (0..height).rev().cartesian_product(0..width) {
-        let r = ((i as f32 / width as f32) * 255.0) as i32;
-        let g = ((j as f32 / height as f32) * 255.0) as i32;
-        let b = (0.2 * 255.0) as i32;
-        println!("{} {} {}", r, g, b);
+        let mut v = Vec3::new(i as f32 / width as f32, j as f32 / height as f32, 0.2);
+        v *= 255.99;
+        println!("{}", v);
     }
 }
--- src/vec3.rs
+++ src/vec3.rs
@@ -1,49 +1,185 @@
-mod vec3 {
-    #[derive(Debug, PartialEq)]
-    pub struct Vec3 {
-        a: [f64; 3],
-    }
+#[derive(Debug, PartialEq)]
+pub struct Vec3 {
+    e: [f32; 3],
+}
 
-    impl Vec3 {
-        fn create(a: &[f64; 3]) -> Vec3 {
-            Vec3 { a: *a }
-        }
-        fn x(&self) -> f64 {
-            self.a[0]
-        }
-        fn y(&self) -> f64 {
-            self.a[1]
-        }
-        fn z(&self) -> f64 {
-            self.a[2]
-        }
-        fn length_squared(&self) -> f64 {
-            self.a.iter().map(|x| x * x).sum()
-        }
-        fn length(&self) -> f64 {
-            let rooted = self.length_squared();
-            rooted * rooted
-        }
+impl Vec3 {
+    pub fn new(x: f32, y: f32, z: f32) -> Vec3 {
+        Vec3 { e: [x, y, z] }
     }
-    use std::ops::{Add, AddAssign, Mul, MulAssign};
-    impl Add for Vec3 {
-        type Output = Vec3;
-        fn add(self, other: Self) -> Self {
-            Vec3 {
-                a: self
-                    .a
-                    .iter()
-                    .zip(other.a.iter())
-                    .map(|(a, b)| a + b)
-                    .collect(),
-            }
+    pub fn x(&self) -> f32 {
+        self.e[0]
+    }
+    pub fn y(&self) -> f32 {
+        self.e[1]
+    }
+    pub fn z(&self) -> f32 {
+        self.e[2]
+    }
+    pub fn length_squared(&self) -> f32 {
+        self.e.iter().map(|x| x * x).sum()
+    }
+    pub fn length(&self) -> f32 {
+        let rooted = self.length_squared();
+        rooted * rooted
+    }
+}
+
+use std::ops::*;
+
+impl Add for Vec3 {
+    type Output = Vec3;
+    fn add(self, other: Vec3) -> Vec3 {
+        Vec3 {
+            e: [
+                self.e[0] + other.e[0],
+                self.e[1] + other.e[1],
+                self.e[2] + other.e[2],
+            ],
         }
     }
 }
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn t_add() {
-        asserteq!(vec3{[1,2]} + vec3{[1,1]},vec3[2,3]);
+
+impl Sub for Vec3 {
+    type Output = Vec3;
+    fn sub(self, other: Vec3) -> Vec3 {
+        Vec3 {
+            e: [
+                self.e[0] - other.e[0],
+                self.e[1] - other.e[1],
+                self.e[2] - other.e[2],
+            ],
+        }
+    }
+}
+
+impl Mul for Vec3 {
+    type Output = Vec3;
+    fn mul(self, other: Vec3) -> Vec3 {
+        Vec3 {
+            e: [
+                self.e[0] * other.e[0],
+                self.e[1] * other.e[1],
+                self.e[2] * other.e[2],
+            ],
+        }
+    }
+}
+
+impl Div for Vec3 {
+    type Output = Vec3;
+    fn div(self, other: Vec3) -> Vec3 {
+        Vec3 {
+            e: [
+                self.e[0] / other.e[0],
+                self.e[1] / other.e[1],
+                self.e[2] / other.e[2],
+            ],
+        }
+    }
+}
+
+impl Mul<Vec3> for f32 {
+    type Output = Vec3;
+    fn mul(self, v: Vec3) -> Vec3 {
+        Vec3 {
+            e: [self * v.e[0], self * v.e[1], self * v.e[2]],
+        }
+    }
+}
+
+impl Div<f32> for Vec3 {
+    type Output = Vec3;
+    fn div(self, t: f32) -> Vec3 {
+        Vec3 {
+            e: [self.e[0] / t, self.e[1] / t, self.e[2] / t],
+        }
+    }
+}
+
+impl Mul<f32> for Vec3 {
+    type Output = Vec3;
+    fn mul(self, t: f32) -> Vec3 {
+        Vec3 {
+            e: [self.e[0] * t, self.e[1] * t, self.e[2] * t],
+        }
+    }
+}
+
+impl Neg for Vec3 {
+    type Output = Vec3;
+    fn neg(self) -> Vec3 {
+        Vec3 {
+            e: [-self.e[0], -self.e[1], -self.e[2]],
+        }
+    }
+}
+
+impl AddAssign for Vec3 {
+    fn add_assign(&mut self, other: Vec3) {
+        self.e[0] += other.e[0];
+        self.e[1] += other.e[1];
+        self.e[2] += other.e[2];
+    }
+}
+
+impl SubAssign for Vec3 {
+    fn sub_assign(&mut self, other: Vec3) {
+        self.e[0] -= other.e[0];
+        self.e[1] -= other.e[1];
+        self.e[2] -= other.e[2];
+    }
+}
+
+impl MulAssign for Vec3 {
+    fn mul_assign(&mut self, other: Vec3) {
+        self.e[0] *= other.e[0];
+        self.e[1] *= other.e[1];
+        self.e[2] *= other.e[2];
+    }
+}
+
+impl DivAssign for Vec3 {
+    fn div_assign(&mut self, other: Vec3) {
+        self.e[0] /= other.e[0];
+        self.e[1] /= other.e[1];
+        self.e[2] /= other.e[2];
+    }
+}
+
+impl MulAssign<f32> for Vec3 {
+    fn mul_assign(&mut self, t: f32) {
+        self.e[0] *= t;
+        self.e[1] *= t;
+        self.e[2] *= t;
+    }
+}
+
+impl DivAssign<f32> for Vec3 {
+    fn div_assign(&mut self, t: f32) {
+        let k = 1.0 / t;
+
+        self.e[0] *= k;
+        self.e[1] *= k;
+        self.e[2] *= k;
+    }
+}
+
+impl Index<usize> for Vec3 {
+    type Output = f32;
+    fn index(&self, i: usize) -> &f32 {
+        &self.e[i]
+    }
+}
+
+impl IndexMut<usize> for Vec3 {
+    fn index_mut(&mut self, i: usize) -> &mut f32 {
+        &mut self.e[i]
+    }
+}
+impl std::fmt::Display for Vec3 {
+    fn fmt(&self, fmt: &mut std::fmt::Formatter<'_>) -> std::result::Result<(), std::fmt::Error> {
+        write!(fmt, "{} {} {}", self.x(), self.y(), self.z())?;
+        Ok(())
     }
 }
